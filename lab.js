6c218ccb0b33fc11b203c4e74eb6aafc422e7d19
moahmed amine mezghich <git@mezghich.com>
2022-09-14 09:14:00 +0100

    ES 6 let var chainage

--- lab.js
+++ lab.js
@@ -1,1 +1,38 @@
-console.log("Bienvenue à REAN");
+//console.log("Bienvenue à REAN");
+
+/*
+function maMethode(duree)  // declaration de méthode
+{
+    console.log("React native pour le dev mobile " + duree);
+}
+
+maMethode(3);  // appel de la méthode*/
+
+// ES 6 
+/*
+function swap(a,b)
+{
+    if(a!=b)
+    {
+        let temp = a;
+        var v = a;
+
+        a = b;
+        b = temp;
+    }
+
+   // console.log(temp);
+    console.log(v);
+}
+
+swap(10,20);*/
+
+let nom = "Amine";
+let formation = "REAN";
+
+function info()
+{
+    return "Formation chez PLB";
+}
+let phrase = `Je suis ${nom}, la formation est : ${formation}, la formation se déroule chez ${info()}`;
+console.log(phrase);
